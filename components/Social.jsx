7350a96170378ec8d5f3cb3b1ffa69a296e6c1ef
sharonerosee <129519148+sharonerosee@users.noreply.github.com>
2024-11-15 10:17:37 +0700

    sosmed template

---
+++ components/Social.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedin />, path: "" },
+  { icon: <FaInstagram />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => (
+        <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Social;
